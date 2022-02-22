30b15421c59b114e394f5ad1f438c7a76c114c35
ayumuohtaki <ayumu0821@gmail.com>
2022-02-22 20:27:19 +0900

    初コミット

---
+++ content-archive.php
@@ -0,0 +1,9 @@
+                       <a class="news-link" href="<?php the_permalink(); ?>">
+                          <div class="news-body">
+                           
+                            <div class="thumnail mask"><?php the_post_thumbnail( 'blog' ); ?></div>
+                            
+                            <time class="release"><?php the_time('Y.m.d'); ?></time>
+                            <p class="title"><?php the_title(); ?></p>
+                          </div>
+                        </a>
---
+++ front-page.php
@@ -0,0 +1,46 @@
+
+<?php get_header(); ?>
+
+    <div class="head-logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/pm_1.png"  class="head-logo-master animate__animated animate__rotateIn animate__slower head-logo-before head-logo-after" alt="PACIFIC MALL DEVELOPMENT" />
+    </div>
+
+   <div id="scroll-cover">
+    <section class="section-contents" id="blog">
+      <div class="wrapper">
+<?php $term_obj = get_term_by( 'slug', 'blog', 'category' ); ?>
+        <span class="section-title-en"><?php the_field( 'english_title', $term_obj->taxonomy. '_'. $term_obj->term_id ); ?></span>
+        <h2 class="section-title animate__animated animate__fadeInUpBig"><?php echo $term_obj->name; ?></h2>
+        <p class="section-lead"><?php echo $term_obj->description; ?></p>
+        <ul class="news-front">
+<?php
+$news_posts = get_specific_posts( 'post' , 'category' , 'blog', 5 );
+if( $news_posts->have_posts() ):
+    while( $news_posts->have_posts() ): $news_posts->the_post();
+?>
+          <li class="news-item">
+            <a class="detail-link" href="<?php the_permalink(); ?>">
+              <div class="common-image">
+                <div class="mask">
+                <?php the_post_thumbnail(); ?>
+                </div>
+              </div>
+              <!-- <time class="time"><?php the_time('Y.m.d'); ?></time> --> <!-- 投稿数が増えてきたら投稿日時を追加 -->
+              <p class="title"><?php the_title(); ?></p>
+              <p class="news-text"><?php echo get_the_excerpt(); ?></p>
+            </a>
+          </li>
+<?php
+    endwhile;
+    wp_reset_postdata();
+endif;
+?>
+          
+        </ul>
+        <div class="section-buttons animate__animated animate__pulse count10">
+          <button type="button" class="button button-ghost zoomInClickTrigger" onclick="javascript:location.href = '<?php echo esc_url( get_term_link( $term_obj ) ); ?>';"><?php echo $term_obj->name; ?>一覧を見る</button>
+        </div>
+      </div>
+    </section>
+   </div>
+<?php get_footer(); ?>
